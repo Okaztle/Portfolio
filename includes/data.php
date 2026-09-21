169b3e517253c0cab86ab6decace237baf5b6c29
Okaztle <50444622+Okaztle@users.noreply.github.com>
2026-09-21 17:01:42 +0100

    Create data.php

---
+++ includes/data.php
@@ -0,0 +1,22 @@
+<?php
+$projects = [
+    [
+        "id" => 1,
+        "title" => "Luminary Studio",
+        "category" => "Web Design & GSAP",
+        "description" => "Animated single-page layout for a luxury photography studio.",
+    ],
+    [
+        "id" => 2,
+        "title" => "The Fish Dock",
+        "category" => "UI/UX & Frontend",
+        "description" => "Responsive 3D menu and information site for a river restaurant.",
+    ],
+    [
+        "id" => 3,
+        "title" => "Echora Template",
+        "category" => "HTML5 / CSS",
+        "description" => "Dark-themed web template tailored for music artists.",
+    ]
+];
+?>
